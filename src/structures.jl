0ae84b051dff306baecac02403bb0f7e727c6516
liviomaya <liviomaya@gmail.com>
2021-02-01 20:59:29 -0800

    remove dpsolution structure

--- src/structures.jl
+++ src/structures.jl
@@ -8,18 +8,6 @@ struct model
     q::Int64
 end
 # m = model(A,B,C,Σ,n,m,q)
-
-struct dpsolution
-    Λ2::Array{Float64,2}
-    Lt2::Array{Float64,2}
-    Mx0::Array{Float64,2}
-    My0::Array{Float64,2}
-    Mz0::Array{Float64,2}
-    αx::Array{Float64,2}
-    αy::Array{Float64,2}
-    αz::Array{Float64,2}
-end
-
 struct solution
     P::Array{Float64,2}
     Q::Array{Float64,2}
