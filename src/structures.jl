ad0d6e2bb153210bf08ae4daa7a43acac013d538
liviomaya <liviomaya@gmail.com>
2021-05-05 15:48:10 -0700

    new structures

--- src/structures.jl
+++ src/structures.jl
@@ -1,17 +1,33 @@
+
+# VAR Model
+# xₜ = P xₜ₋₁ + Q ϵₜ, 
+# cov(ϵₜ) = Σ
+struct VARModel
+    P::Array{Float64,2}
+    Q::Array{Float64,2}
+    Σ::Array{Float64,2}
+    n::Int64
+    q::Int64
+end
+
+VARModel(P,Q,Σ) = VARModel(P, Q, Σ, size(P,1), size(Σ,1))
+
+# Linear Rational Expectations Model
+# A (xₜ ; Eₜ yₜ₊₁) = B (xₜ₋₁ ; yₜ) + C ϵₜ
+# cov(ϵₜ) = Σ
 struct Model
-    A::Array{Float64,2}
+    A::Array{Float64,2} 
     B::Array{Float64,2}
     C::Array{Float64,2}
     Σ::Array{Float64,2}
-    n::Int64
-    m::Int64
-    q::Int64
-end
-# m = Model(A,B,C,Σ,n,m,q)
-struct Solution
-    P::Array{Float64,2}
-    Q::Array{Float64,2}
+    n::Int64 # number of states
+    m::Int64 # number of forward-looking or static variables
+    q::Int64 # number of shocks
+    λ::Array{Float64,1} # system eigenvalues
+    S::VARModel # solution in VAR form
     flag_rank::Bool
     flag_complex::Bool
 end
-# sol = Solution(P, Q, flag_rank, flag_complex)
+# m = Model(A,B,C,Σ,n,m,q,λ,S,flag_rank,flag_complex)
+
+# Model(A,B,C,Σ,n)
